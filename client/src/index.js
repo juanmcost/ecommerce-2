507739cbcd0f15ef00adfed0db967ca24f38144a
juanmcost <jmcostanzo@gmail.com>
2021-11-08 16:44:31 -0300

    ruteo agregado

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 const theme = extendTheme({ components: { Button: { baseStyle: { _focus: { boxShadow: 'none' } } } } });
 
 ReactDOM.render(
-    <ChakraProvider theme={theme}>
-        <App />
-    </ChakraProvider>,
-
+    <React.StrictMode>
+        <BrowserRouter>
+            <ChakraProvider theme={theme}>
+                <App />
+            </ChakraProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
     document.getElementById('root')
 );
