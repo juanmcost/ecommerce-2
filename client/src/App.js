80e4b3dffc47535ae30accb90f48182c991fda1d
MartinMza <medizam.99@gmail.com>
2021-11-08 17:55:57 -0300

    maquetado components, containers, views

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { SocialProfileWithImage } from './components/card';
-import Carousel from './components/Carousel';
-import { SimpleCard } from './components/login';
-import { Route, Routes } from 'react-router-dom';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { SocialProfileWithImage } from "./components/Card";
+import Carousel from "./components/Carousel";
+import { SimpleCard } from "./components/login";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-    return (
-        <div>
-        <Navbar />
-        <Routes>
-            <Route path = '/home' element = {<Carousel />}/>
-            <Route path = '/login' element = {<SimpleCard />} />
-            <Route path = '/signIn' element = {<></>} />
-            <Route path = '/profile' element = {<SocialProfileWithImage />} />
-            <Route path = '/myCart' element = {<></>} />
-            <Route path = {`/categories/categoryId`} element = {<>{/* <productsByCategory id={categoryId} /> */}</>} /> //categoryId deberia ser un estado
-        </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Carousel />} />
+        <Route path="/login" element={<SimpleCard />} />
+        <Route path="/signIn" element={<></>} />
+        <Route path="/profile" element={<SocialProfileWithImage />} />
+        <Route path="/myCart" element={<></>} />
+        <Route
+          path={`/categories/categoryId`}
+          element={<>{/* <productsByCategory id={categoryId} /> */}</>}
+        />{" "}
+        //categoryId deberia ser un estado
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-const theme = extendTheme({ components: { Button: { baseStyle: { _focus: { boxShadow: 'none' } } } } });
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import store from "./store/store";
+const theme = extendTheme({
+  components: { Button: { baseStyle: { _focus: { boxShadow: "none" } } } },
+});
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <ChakraProvider theme={theme}>
-                <App />
-            </ChakraProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
