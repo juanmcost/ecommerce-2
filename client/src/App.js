9bbc08a32f8a42416e934305797677a578299789
juanmcost <jmcostanzo@gmail.com>
2021-11-09 13:28:08 -0300

    agrego request a grid

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { SocialProfileWithImage } from "./components/Card";
 import Carousel from "./components/Carousel";
 import { SimpleCard } from "./components/Login";
 import { Route, Routes } from "react-router-dom";
+import ProductsGrid from "./components/Grid";
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
         <Route path="/login" element={<SimpleCard />} />
         <Route path="/signIn" element={<></>} />
         <Route path="/profile" element={<SocialProfileWithImage />} />
-        <Route path="/myCart" element={<></>} />
+        <Route path="/myCart" element={<ProductsGrid/>} />
         <Route
           path={`/categories/categoryId`}
           element={<>{/* <productsByCategory id={categoryId} /> */}</>}
--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -1,17 +1,25 @@
 import { Grid, GridItem, Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Item from "./Item";
 
 
-const productsGrid = () => {
-    const aux = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const ProductsGrid = () => {
+    const res = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     
+    /* const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("")
+        .then(res => setProducts(res.data));
+    }, []) */
+
     return (
         <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-            {aux.map(num => (
-                <Item/>
+            {res.map(elem => (
+                <Item item={elem}/>
             ))}
         </Grid>
     )
 }
 
-export default productsGrid;
+export default ProductsGrid;
