2ade20a1509d1c111f8be0d5e656158171169315
juanmcost <jmcostanzo@gmail.com>
2021-11-09 16:55:20 -0300

    empiezo shopCart

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Carousel from "./components/Carousel";
 import { SimpleCard } from "./components/Login";
 import { Route, Routes } from "react-router-dom";
 import ProductsGrid from "./components/Grid";
+import ShopCart from "./components/ShopCart";
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
         <Route path="/login" element={<SimpleCard />} />
         <Route path="/signIn" element={<></>} />
         <Route path="/profile" element={<SocialProfileWithImage />} />
-        <Route path="/myCart" element={<ProductsGrid/>} />
+        <Route path="/myCart" element={<ShopCart />} />
         <Route
           path={`/categories/categoryId`}
           element={<>{/* <productsByCategory id={categoryId} /> */}</>}
---
+++ client/src/components/ShopCart.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+
+const ShopCart = () => {
+    const [user,setUser] = useState({});
+    
+    useEffect(() => {
+        //pa probar:
+        const testObject = {username: "ale"}
+        localStorage.setItem('loggedUser', JSON.stringify(testObject));
+        //esto si hiría:
+        const jsonUser = localStorage.getItem('loggedUser');
+        if (jsonUser) setUser(JSON.parse(jsonUser))
+    }, [])
+    return (
+        <h1>carrito de {user.username}</h1>
+    )
+}
+
+export default ShopCart
