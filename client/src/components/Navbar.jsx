53968e136febb544a92c96054b6480742b32f5de
juanmcost <jmcostanzo@gmail.com>
2021-11-09 12:48:24 -0300

    agrego categorias a navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,17 +15,28 @@ import {
     Stack,
     useColorMode,
     Center,
+    Divider,
+    Collapse,
+    Popover,
+    PopoverContent,
+    PopoverTrigger
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 export default function Navbar() {
     const { colorMode, toggleColorMode } = useColorMode();
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
+
+    const subCategories = {
+        cellphones: ["samsung"],
+        computers: ["LG", "tu vieja"]
+    }
+
     return (
         <>
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
                 <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                    <Box>Logo</Box>
+                    <Box>logo</Box>
 
                     <Flex alignItems={'center'}>
                         <Stack direction={'row'} spacing={7}>
@@ -56,6 +67,23 @@ export default function Navbar() {
                         </Stack>
                     </Flex>
                 </Flex>
+                <Divider orientation="horizontal"/>
+                {Object.keys(subCategories).map((category, i) => (
+                    <Popover trigger={"hover"} key={i}>
+                        <PopoverTrigger bg="none" on>
+                            <Button bg="none">
+                                {category}
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <Stack>
+                                {subCategories[category].map((sub, i) => (
+                                    <Box key={i}>{sub}</Box>
+                                ))}
+                            </Stack>
+                        </PopoverContent>
+                    </Popover>
+                ))}
             </Box>
         </>
     );
