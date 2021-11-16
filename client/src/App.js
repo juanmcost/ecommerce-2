e0561271c9219aad7d54b9bcc942964055f333e3
juanmcost <jmcostanzo@gmail.com>
2021-11-16 16:38:06 -0300

    solving conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ShopCartDB from "./components/ShopCartDB";
 import { NewProduct } from "./containers/NewProduct";
 import { ModifyProduct } from "./containers/ModifyProduct";
@@ -20,28 +19,7 @@ import ShopCart from "./components/ShopCart";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "./store/user";
-=======
-import ShopCartDB from './components/ShopCartDB';
-import { NewProduct } from './containers/NewProduct';
-import { ModifyProduct } from './containers/ModifyProduct';
-import React, { useEffect } from 'react';
-import Home from './views/Home';
-import Navbar from './components/Navbar';
-import { SocialProfileWithImage } from './components/Card';
-import Carousel from './components/Carousel';
-import Login from './views/Login';
-import Register from './views/Register';
-import Profile from './views/Profile';
-import Admin from './views/Admin.jsx';
-import { Route, Routes } from 'react-router-dom';
-import Article from './views/Article';
-import MyProducts from './views/MyProducts';
-import ShopCart from './components/ShopCart';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUser } from './store/user';
 import AdminPanel from './components/Admin/AdminPanel';
->>>>>>> ff4e2a264c95ed522663d488ce46068ea28d88f8
 
 const App = () => {
     const dispatch = useDispatch();
@@ -54,13 +32,13 @@ const App = () => {
         });
     }, []);
 
-<<<<<<< HEAD
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/v2" element={<AdminPanel />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -88,33 +66,6 @@ const App = () => {
       </Routes>
     </div>
   );
-=======
-    return (
-        <div>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/admin/v2" element={<AdminPanel />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/myProducts" element={<MyProducts />} />
-                <Route path="/myCart" element={<ShopCart />} />
-                <Route path="/myProducts/newProduct" element={<NewProduct />} />
-                <Route path="/myProducts/ModifyProduct/:id" element={<ModifyProduct />} />
-                <Route path={`/${user.username}/myCart`} element={<ShopCartDB />} />
-                <Route path={`/cart`} element={<ShopCart />} />
-                <Route
-                    path={`/categories/categoryId`}
-                    element={<>{/* <productsByCategory id={categoryId} /> */}</>}
-                />{' '}
-                <Route exact path="/articles/:id" element={<Article />} />
-            </Routes>
-        </div>
-    );
->>>>>>> ff4e2a264c95ed522663d488ce46068ea28d88f8
 };
 
 export default App;
