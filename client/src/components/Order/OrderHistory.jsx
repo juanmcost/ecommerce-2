0a8796cd17573d9c78f56c59b84011885a36f1f6
MartinMza <medizam.99@gmail.com>
2021-11-18 11:13:15 -0300

    historial ordenes

--- client/src/components/Order/OrderHistory.jsx
+++ client/src/components/Order/OrderHistory.jsx
@@ -1,30 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import OrderList from './OrderList'
-import axios from "axios"
-
+import React, { useEffect, useState } from "react";
+import OrderList from "./OrderList";
+import axios from "axios";
+import { Flex, Heading, Stack } from "@chakra-ui/react";
 
 const OrderHistory = () => {
+  const [order, setOrder] = useState([]);
 
-    const [order, setOrder] = useState([])
+  useEffect(() => {
+    fetchOrders();
+    async function fetchOrders() {
+      const { data } = await axios.get("/api/order/history");
+      console.log("datazo", data);
+      if (data.length) setOrder(data);
+    }
+  }, []);
+  console.log(`order`, order);
+  return (
+    <div>
+      <Flex align={"center"} justify={"center"}>
+        <Stack spacing={8} maxW={900} py={12}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>My Orders</Heading>
+          </Stack>
+          {order
+            ? order.map((elem, i) => <OrderList key={i} item={elem} />)
+            : null}
+        </Stack>
+      </Flex>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        fetchOrders();
-        async function fetchOrders() {
-            const { data } = await axios.get('/api/order/history');
-            console.log('datazo', data)
-            if (data.length) setOrder(data);
-
-        }
-    }, []);
-    console.log(`order`, order)
-    return (
-        <div>
-            <h1>Order History </h1>
-            {order ? (
-        order.map((elem, i) => <OrderList key={i} item={elem} />)
-      ) : null}
-        </div>
-    )
-}
-
-export default OrderHistory
+export default OrderHistory;
